ab625c34b7fb1cc45ff185edbadb059cd6bf01e9
Igor Antonov <igor.antonov@studiodesigner.com>
2025-05-26 01:53:57 -0400

    Fixing Module Version of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule ({
   declarations: [AppComponent],
-bootstrap: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent
+  ],
 })
 
 
