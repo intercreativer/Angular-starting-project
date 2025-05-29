5a13f83162bd08e744b73ccee699eb7c9122027b
Igor Antonov <igor.antonov@studiodesigner.com>
2025-05-29 01:34:40 -0400

    Continue with modules. Still doesn't work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { HeaderComponent } from './header/header.component';
-import { CardComponent } from './shared/card/card.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+
 @NgModule ({
-  declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent, TasksComponent, NewTaskComponent, TaskComponent],
+  declarations: [
+    AppComponent, 
+    HeaderComponent, 
+    UserComponent,
+   ],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    TasksModule
   ],
 })
 
